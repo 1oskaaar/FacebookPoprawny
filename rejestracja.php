91961870c58a8278632dd1cb718421bea0eb874b
1oskaaar <kloskowskioskar@wp.pl>
2024-05-21 23:06:48 +0200

    zmiana profile php

--- rejestracja.php
+++ rejestracja.php
@@ -16,6 +16,7 @@ if(isset($_REQUEST['email'])&& isset($_REQUEST['password'])) {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Formularz rejestracji</title>
+    <link rel="stylesheet" href="styllogowania.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </head>
@@ -30,6 +31,9 @@ if(isset($_REQUEST['email'])&& isset($_REQUEST['password'])) {
                 <input type="password" class="from-controll mb-3" name="password" id="passwordInput">
                 <button type="submit" class="btn btn-primary w-100 mt-3">Zajerestruj</button>
             </form>
+            <a href="index.php">
+                <button class="btn btn-primary w-100 mt-3">Powrót</button>
+                </a>
             <?php
             if (isset($result)) {
                 if ($result) {
@@ -40,7 +44,7 @@ if(isset($_REQUEST['email'])&& isset($_REQUEST['password'])) {
             }
 
             ?>
-.
+
         </div>
 </div>
 </body>
